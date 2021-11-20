26b2c089131e5ebd32535f6211b5301c8de4d725
Explo <eskot98@gmail.com>
2021-11-20 14:22:51 +0200

    Translated questions to FI via google translate

--- questions.js
+++ questions.js
@@ -1,48 +1,305 @@
-questions =  [
-    {"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"right": 1}},
-    {"id": 1, "question": "Charity is a better way of helping those in need than social welfare.", "effects": {"right": 1}},
-    {"id": 2, "question": "Wages are always fair, as employers know best what a worker's labour is worth.", "effects": {"right": 1}},
-    {"id": 3, "question": "It is \"human nature\" to be greedy.", "effects": {"right": 1}},
-    {"id": 4, "question": "\"Exploitation\" is an outdated term, as the struggles of 1800s capitalism don't exist anymore.", "effects": {"right": 1}},
-    {"id": 5, "question": "Communism is an ideal that can never work in practice.", "effects": {"right": 1}},
-    {"id": 6, "question": "Taxation of the wealthy is a bad idea, society would be better off without it.", "effects": {"right": 1}},
-    {"id": 7, "question": "The harder you work, the more you progress up the social ladder.", "effects": {"right": 1}},
-    {"id": 8, "question": "Organisations and corporations cannot be trusted and need to be regulated by the government.", "effects": {"right": -1}},
-    {"id": 9, "question": "A government that provides for everyone is an inherently good idea.", "effects": {"right": -1}},
-    {"id": 10, "question": "The current welfare system should be expanded to further combat inequality.", "effects": {"right": -1}},
-    {"id": 11, "question": "Land should not be a commodity to be bought and sold.", "effects": {"right": -1}},
-    {"id": 12, "question": "All industry and the bank should be nationalised.", "effects": {"right": -1}},
-    {"id": 13, "question": "Class is the primary division of society.", "effects": {"right": -1}},
-    {"id": 14, "question": "Economic inequality is too high in the world.", "effects": {"right": -1}},
-    {"id": 15, "question": "Sometimes it is right that the government may spy on its citizens to combat extremists and terrorists.", "effects": {"auth": 1}},
-    {"id": 16, "question": "Authority figures, if morally correct, are a good thing for society.", "effects": {"auth": 1}},
-    {"id": 17, "question": "Strength is necessary for any government to succeed.", "effects": {"auth": 1}},
-    {"id": 18, "question": "Only the government can fairly and effectively regulate organisations.", "effects": {"auth": 1}},
-    {"id": 19, "question": "Society requires structure and bureaucracy in order to function.", "effects": {"auth": 1}},
-    {"id": 20, "question": "Mandatory IDs should be used to ensure public safety.", "effects": {"auth": 1}},
-    {"id": 21, "question": "In times of crisis, safety becomes more important than civil liberties.", "effects": {"auth": 1}},
-    {"id": 22, "question": "If you have nothing to hide, you have nothing to fear.", "effects": {"auth": 1}},
-    {"id": 23, "question": "The government should be less involved in the day to day life of its citizens.", "effects": {"auth": -1}},
-    {"id": 24, "question": "Without democracy, a society is nothing.", "effects": {"auth": -1}},
-    {"id": 25, "question": "Jury nullification should be legal.", "effects": {"auth": -1}},
-    {"id": 26, "question": "The smaller the government, the freer the people.", "effects": {"auth": -1}},
-    {"id": 27, "question": "The government should, at most, provide emergency services and law enforcement.", "effects": {"auth": -1}},
-    {"id": 28, "question": "The police was not made to protect the people, but to uphold the status-quo by force.", "effects": {"auth": -1}},
-    {"id": 29, "question": "State schools are a bad idea because our state shouldn't be influencing our children.", "effects": {"auth": -1}},
-    {"id": 30, "question": "Two consenting individuals should be able to do whatever they want with each other, even if it makes me uncomfortable.", "effects": {"prog": 1}},
-    {"id": 31, "question": "An individual's body is their own property, and they should be able to do anything they desire to it.", "effects": {"prog": 1}},
-    {"id": 32, "question": "A person should be able to worship whomever or whatever they want.", "effects": {"prog": 1}},
-    {"id": 33, "question": "Nudism is perfectly natural.", "effects": {"prog": 1}},
-    {"id": 34, "question": "Animals deserve certain universal rights.", "effects": {"prog": 1}},
-    {"id": 35, "question": "Gender is a social construct, not a natural state of affairs.", "effects": {"prog": 1}},
-    {"id": 36, "question": "Laws based on cultural values, rather than ethical ones, aren't justice.", "effects": {"prog": 1}},
-    {"id": 37, "question": "Autonomy of body extends even to minors, the mentally ill, and serious criminals.", "effects": {"prog": 1}},
-    {"id": 38, "question": "Homosexuality is against my values.", "effects": {"prog": -1}},
-    {"id": 39, "question": "Transgender individuals should not be able to adopt children.", "effects": {"prog": -1}},
-    {"id": 40, "question": "Drugs are harmful and should be banned.", "effects": {"prog": -1}},
-    {"id": 41, "question": "The death penalty should exist for certain crimes.", "effects": {"prog": -1}},
-    {"id": 42, "question": "Victimless crimes should still be punished.", "effects": {"prog": -1}},
-    {"id": 43, "question": "One cannot be moral without religion.", "effects": {"prog": -1}},
-    {"id": 44, "question": "Parents should hold absolute power over their children, as they are older and more experienced.", "effects": {"prog": -1}},
-    {"id": 45, "question": "Multiculturalism is bad.", "effects": {"prog": -1}}
+// questions =  [
+//     {"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"right": 1}},
+//     {"id": 1, "question": "Charity is a better way of helping those in need than social welfare.", "effects": {"right": 1}},
+//     {"id": 2, "question": "Wages are always fair, as employers know best what a worker's labour is worth.", "effects": {"right": 1}},
+//     {"id": 3, "question": "It is \"human nature\" to be greedy.", "effects": {"right": 1}},
+//     {"id": 4, "question": "\"Exploitation\" is an outdated term, as the struggles of 1800s capitalism don't exist anymore.", "effects": {"right": 1}},
+//     {"id": 5, "question": "Communism is an ideal that can never work in practice.", "effects": {"right": 1}},
+//     {"id": 6, "question": "Taxation of the wealthy is a bad idea, society would be better off without it.", "effects": {"right": 1}},
+//     {"id": 7, "question": "The harder you work, the more you progress up the social ladder.", "effects": {"right": 1}},
+//     {"id": 8, "question": "Organisations and corporations cannot be trusted and need to be regulated by the government.", "effects": {"right": -1}},
+//     {"id": 9, "question": "A government that provides for everyone is an inherently good idea.", "effects": {"right": -1}},
+//     {"id": 10, "question": "The current welfare system should be expanded to further combat inequality.", "effects": {"right": -1}},
+//     {"id": 11, "question": "Land should not be a commodity to be bought and sold.", "effects": {"right": -1}},
+//     {"id": 12, "question": "All industry and the bank should be nationalised.", "effects": {"right": -1}},
+//     {"id": 13, "question": "Class is the primary division of society.", "effects": {"right": -1}},
+//     {"id": 14, "question": "Economic inequality is too high in the world.", "effects": {"right": -1}},
+//     {"id": 15, "question": "Sometimes it is right that the government may spy on its citizens to combat extremists and terrorists.", "effects": {"auth": 1}},
+//     {"id": 16, "question": "Authority figures, if morally correct, are a good thing for society.", "effects": {"auth": 1}},
+//     {"id": 17, "question": "Strength is necessary for any government to succeed.", "effects": {"auth": 1}},
+//     {"id": 18, "question": "Only the government can fairly and effectively regulate organisations.", "effects": {"auth": 1}},
+//     {"id": 19, "question": "Society requires structure and bureaucracy in order to function.", "effects": {"auth": 1}},
+//     {"id": 20, "question": "Mandatory IDs should be used to ensure public safety.", "effects": {"auth": 1}},
+//     {"id": 21, "question": "In times of crisis, safety becomes more important than civil liberties.", "effects": {"auth": 1}},
+//     {"id": 22, "question": "If you have nothing to hide, you have nothing to fear.", "effects": {"auth": 1}},
+//     {"id": 23, "question": "The government should be less involved in the day to day life of its citizens.", "effects": {"auth": -1}},
+//     {"id": 24, "question": "Without democracy, a society is nothing.", "effects": {"auth": -1}},
+//     {"id": 25, "question": "Jury nullification should be legal.", "effects": {"auth": -1}},
+//     {"id": 26, "question": "The smaller the government, the freer the people.", "effects": {"auth": -1}},
+//     {"id": 27, "question": "The government should, at most, provide emergency services and law enforcement.", "effects": {"auth": -1}},
+//     {"id": 28, "question": "The police was not made to protect the people, but to uphold the status-quo by force.", "effects": {"auth": -1}},
+//     {"id": 29, "question": "State schools are a bad idea because our state shouldn't be influencing our children.", "effects": {"auth": -1}},
+//     {"id": 30, "question": "Two consenting individuals should be able to do whatever they want with each other, even if it makes me uncomfortable.", "effects": {"prog": 1}},
+//     {"id": 31, "question": "An individual's body is their own property, and they should be able to do anything they desire to it.", "effects": {"prog": 1}},
+//     {"id": 32, "question": "A person should be able to worship whomever or whatever they want.", "effects": {"prog": 1}},
+//     {"id": 33, "question": "Nudism is perfectly natural.", "effects": {"prog": 1}},
+//     {"id": 34, "question": "Animals deserve certain universal rights.", "effects": {"prog": 1}},
+//     {"id": 35, "question": "Gender is a social construct, not a natural state of affairs.", "effects": {"prog": 1}},
+//     {"id": 36, "question": "Laws based on cultural values, rather than ethical ones, aren't justice.", "effects": {"prog": 1}},
+//     {"id": 37, "question": "Autonomy of body extends even to minors, the mentally ill, and serious criminals.", "effects": {"prog": 1}},
+//     {"id": 38, "question": "Homosexuality is against my values.", "effects": {"prog": -1}},
+//     {"id": 39, "question": "Transgender individuals should not be able to adopt children.", "effects": {"prog": -1}},
+//     {"id": 40, "question": "Drugs are harmful and should be banned.", "effects": {"prog": -1}},
+//     {"id": 41, "question": "The death penalty should exist for certain crimes.", "effects": {"prog": -1}},
+//     {"id": 42, "question": "Victimless crimes should still be punished.", "effects": {"prog": -1}},
+//     {"id": 43, "question": "One cannot be moral without religion.", "effects": {"prog": -1}},
+//     {"id": 44, "question": "Parents should hold absolute power over their children, as they are older and more experienced.", "effects": {"prog": -1}},
+//     {"id": 45, "question": "Multiculturalism is bad.", "effects": {"prog": -1}}
+// ];
+
+questions = [
+  {
+    id: 0,
+    question:
+      "Liiketoiminnan vapaus on paras k\u00e4yt\u00e4nn\u00f6llinen tapa, jolla yhteiskunta voi menesty\u00e4.",
+    effects: { right: 1 },
+  },
+  {
+    id: 1,
+    question:
+      "Hyv\u00e4ntekev\u00e4isyys on parempi tapa auttaa tarvitsevia kuin sosiaaliturva.",
+    effects: { right: 1 },
+  },
+  {
+    id: 2,
+    question:
+      "Palkat ovat aina oikeudenmukaisia, sill\u00e4 ty\u00f6nantajat tiet\u00e4v\u00e4t parhaiten, mit\u00e4 ty\u00f6ntekij\u00e4n ty\u00f6voima on arvoinen.",
+    effects: { right: 1 },
+  },
+  {
+    id: 3,
+    question: 'Se on "ihmisen luonne" olla ahne.',
+    effects: { right: 1 },
+  },
+  {
+    id: 4,
+    question:
+      '"Hy\u00f6dynt\u00e4minen" on vanhentunut termi, koska 1800-luvun kamppailut eiv\u00e4t en\u00e4\u00e4 ole.',
+    effects: { right: 1 },
+  },
+  {
+    id: 5,
+    question:
+      "Kommunismi on ihanteellinen, joka ei voi koskaan toimia k\u00e4yt\u00e4nn\u00f6ss\u00e4.",
+    effects: { right: 1 },
+  },
+  {
+    id: 6,
+    question:
+      "Verotus varakkaasta on huono idea, yhteiskunta olisi parempi ilman sit\u00e4.",
+    effects: { right: 1 },
+  },
+  {
+    id: 7,
+    question:
+      "Mit\u00e4 vaikeampaa ty\u00f6skentelet, sit\u00e4 enemm\u00e4n edistyt sosiaaliset tikkaat.",
+    effects: { right: 1 },
+  },
+  {
+    id: 8,
+    question:
+      "J\u00e4rjest\u00f6j\u00e4 ja yrityksi\u00e4 ei voida luottaa ja hallitus on s\u00e4\u00e4nnelt\u00e4v\u00e4.",
+    effects: { right: -1 },
+  },
+  {
+    id: 9,
+    question: "Hallitus, joka tarjoaa kaikille, on luonnostaan hyv\u00e4 idea.",
+    effects: { right: -1 },
+  },
+  {
+    id: 10,
+    question:
+      "Nykyist\u00e4 hyvinvointij\u00e4rjestelm\u00e4\u00e4 olisi laajennettava ep\u00e4tasa-arvon torjumiseksi.",
+    effects: { right: -1 },
+  },
+  {
+    id: 11,
+    question: "Maa ei pit\u00e4isi olla hy\u00f6dyke ostaa ja myyd\u00e4.",
+    effects: { right: -1 },
+  },
+  {
+    id: 12,
+    question: "Kaikki teollisuus ja pankki olisi kansallista.",
+    effects: { right: -1 },
+  },
+  {
+    id: 13,
+    question: "Luokka on yhteiskunnan ensisijainen jako.",
+    effects: { right: -1 },
+  },
+  {
+    id: 14,
+    question: "Taloudellinen ep\u00e4tasa-arvo on liian korkea maailmassa.",
+    effects: { right: -1 },
+  },
+  {
+    id: 15,
+    question:
+      "Joskus hallitus voi olla vakooja kansalaisten torjumiseksi \u00e4\u00e4riliikkeisiin ja terroristeihin.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 16,
+    question:
+      "Viranomaisten luvut, jos moraalisesti oikein, ovat hyv\u00e4 asia yhteiskunnalle.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 17,
+    question:
+      "Vahvuus on v\u00e4ltt\u00e4m\u00e4t\u00f6nt\u00e4, jotta kaikki hallitukset menestyv\u00e4t.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 18,
+    question:
+      "Vain hallitus voi s\u00e4\u00e4t\u00e4\u00e4 oikeudenmukaisesti ja tehokkaasti organisaatioita.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 19,
+    question:
+      "Yhteiskunta edellytt\u00e4\u00e4 rakennetta ja byrokratiaa toimimaan.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 20,
+    question:
+      "Pakollisia tunnuksia olisi k\u00e4ytett\u00e4v\u00e4 yleisen turvallisuuden varmistamiseksi.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 21,
+    question:
+      "Kriisin aikoina turvallisuus tulee t\u00e4rke\u00e4mm\u00e4ksi kuin kansalaisvapauksien.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 22,
+    question:
+      "Jos sinulla ei ole mit\u00e4\u00e4n piilottavaa, sinulla ei ole mit\u00e4\u00e4n pel\u00e4t\u00e4.",
+    effects: { auth: 1 },
+  },
+  {
+    id: 23,
+    question:
+      "Hallituksen olisi oltava v\u00e4hemm\u00e4n mukana kansalaisten p\u00e4ivitt\u00e4isess\u00e4 el\u00e4m\u00e4ss\u00e4.",
+    effects: { auth: -1 },
+  },
+  {
+    id: 24,
+    question: "Ilman demokratiaa yhteiskunta ei ole mit\u00e4\u00e4n.",
+    effects: { auth: -1 },
+  },
+  {
+    id: 25,
+    question: "Tuomariston joutumisen pit\u00e4isi olla laillinen.",
+    effects: { auth: -1 },
+  },
+  {
+    id: 26,
+    question: "Mit\u00e4 pienempi hallitus, vapauttaa ihmisi\u00e4.",
+    effects: { auth: -1 },
+  },
+  {
+    id: 27,
+    question:
+      "Hallituksen olisi enint\u00e4\u00e4n h\u00e4t\u00e4palveluja ja lainvalvontaviranomaisia.",
+    effects: { auth: -1 },
+  },
+  {
+    id: 28,
+    question:
+      "Poliisi ei ollut suojella ihmisi\u00e4, vaan yll\u00e4pit\u00e4\u00e4 status-quo voimakkaasti.",
+    effects: { auth: -1 },
+  },
+  {
+    id: 29,
+    question:
+      "Valtion koulut ovat huono idea, koska valtiomme ei pit\u00e4isi vaikuttaa lapsemme.",
+    effects: { auth: -1 },
+  },
+  {
+    id: 30,
+    question:
+      "Kaksi suostumusta olisi voitava tehd\u00e4 mit\u00e4 he haluavat toistensa kanssa, vaikka se saa minut ep\u00e4mukavaksi.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 31,
+    question:
+      "Yksil\u00f6n ruumis on oma omaisuutensa, ja heid\u00e4n pit\u00e4isi pysty\u00e4 tekem\u00e4\u00e4n mit\u00e4 tahansa, mit\u00e4 he haluavat.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 32,
+    question:
+      "Henkil\u00f6n pit\u00e4isi pysty\u00e4 palvomaan kuka tahansa tai mit\u00e4 he haluavat.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 33,
+    question: "Nudismi on t\u00e4ysin luonnollinen.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 34,
+    question: "El\u00e4imet ansaitsevat tietyt yleiset oikeudet.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 35,
+    question: "Sukupuoli on sosiaalinen rakentaminen, ei luonnollinen tilanne.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 36,
+    question:
+      "Kulttuuritarvoihin perustuvat lait, eettiset, eiv\u00e4t ole oikeudenmukaisia.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 37,
+    question:
+      "Rungon autonomia ulottuu my\u00f6s alaik\u00e4isille, henkisesti sairaille ja vakaville rikollisille.",
+    effects: { prog: 1 },
+  },
+  {
+    id: 38,
+    question: "Homoseksuaalisuus on arvoja vastaan.",
+    effects: { prog: -1 },
+  },
+  {
+    id: 39,
+    question:
+      "Transsukupuolten ei pit\u00e4isi pysty\u00e4 hyv\u00e4ksym\u00e4\u00e4n lapsia.",
+    effects: { prog: -1 },
+  },
+  {
+    id: 40,
+    question: "Huumeet ovat haitallisia ja ne olisi kiellett\u00e4v\u00e4.",
+    effects: { prog: -1 },
+  },
+  {
+    id: 41,
+    question: "Kuolemanrangaistus olisi olemassa tiettyihin rikoksiin.",
+    effects: { prog: -1 },
+  },
+  {
+    id: 42,
+    question: "Uskomaton rikoksia olisi viel\u00e4 rangaistava.",
+    effects: { prog: -1 },
+  },
+  {
+    id: 43,
+    question: "Ei voi olla moraalinen ilman uskontoa.",
+    effects: { prog: -1 },
+  },
+  {
+    id: 44,
+    question:
+      "Vanhempien pit\u00e4isi olla ehdoton valta lapsensa p\u00e4\u00e4ll\u00e4, koska ne ovat vanhempia ja kokeneempia.",
+    effects: { prog: -1 },
+  },
+  { id: 45, question: "Monikulttuurisuus on huono.", effects: { prog: -1 } },
 ];
